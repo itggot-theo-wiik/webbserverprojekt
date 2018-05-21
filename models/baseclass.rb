24a35bb625d83bcfcc9eefcc96ebd1bedf8f7188
itggot-theo-wiik <theo.wiik@itggot.se>
2018-05-21 09:03:58 +0200

    close #20 yardoc (some things are a little weirdly documented)

--- models/baseclass.rb
+++ models/baseclass.rb
@@ -13,6 +13,11 @@ class MyBaseclass
         @columns
     end
 
+    # Gets and returns a object of a specific row which is selected by 'id' in a specific given '@table_name'.
+    #
+    # @param id [Integer] An integer declaring which row to get from the database.
+    # @params table_name [String] A string declaring which table to search in
+    # @return [MyBaseclass] An object of the row
     def self.get(id)
         db = SQLite3::Database.open('db/db.sqlite')
         result = db.execute("SELECT * FROM #{@table_name} WHERE id IS ?", id).first
@@ -20,6 +25,11 @@ class MyBaseclass
     end
 
     def self.all()
+    # Gets and returns a list of objects of all rows in a specific given 'database'. With sorting
+    #
+    # @param table_name [String] A string declaring what database to search in
+    # @param sort [String] A string declaring in what order to return the list of objects. "reverse" will return the array in an reversed order, if nothing is given, it will be false
+    # @return [Array] A list of objects of all the rows
     def self.all(sort = nil)
         db = SQLite3::Database.open('db/db.sqlite')
         all = db.execute("SELECT * FROM #{@table_name}")
@@ -44,6 +54,10 @@ class MyBaseclass
 
     #Comment.create(comment: "hej", img_url: "slkdjfldkjf")
     def self.create(hash)
+    # Attribute checker thing
+    #
+    # @param hash [Hash] A hash with the column names
+    # @return [Boolean] false if something does not meet up to the requirements. Otherwise a hash with the key default: which contains a list of all the default values. And a key of the hash
     def self.attribute_checker(hash)
         db = SQLite3::Database.open('db/db.sqlite')
         
@@ -97,6 +111,11 @@ class MyBaseclass
         return {default: default, hash: hash}
         # return default
     end
+
+    # Creates a row in the database 
+    #
+    # @param hash [hash] A hash with all the column names as keys, where the key is the column name and the value is the value of the column name
+    # @return [Boolean] True or False. True if the db creation is successfull and false if not.
         attribute_checker = self.attribute_checker(hash)
         return false if !attribute_checker
 
@@ -114,6 +133,13 @@ class MyBaseclass
         return true
     end
 
+    # Updates a cell in a table
+    #
+    # @param search_column [String] A string of the name of the column where you are looking for a specific value.
+    # @param search_value [String] A string or index declaring what value you are searching for, so you change the correct row
+    # @param target_column [String] Which column to change
+    # @param value [String] The new value
+    # @return [Boolean] True or False. True if the db creation is successfull and false if not.
     def self.update(search_column, search_value, target_column, value)
         db = SQLite3::Database.open('db/db.sqlite')
         db.execute("UPDATE #{@table_name} SET #{target_column} = ? WHERE #{search_column} IS ?", [value, search_value])
